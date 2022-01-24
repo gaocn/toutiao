77a013c1150ed55a190bf2504c1332cc320b3b3a
gaocn <gaowenwencn@163.com>
2022-01-24 23:43:48 +0800

    添加iconfont图标和图片素材

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div>
+      <i class="iconfont  toutiao-dianzan2"></i>
+      <i class="iconfont  toutiao-fenxiang"></i>
+    </div>
     <router-view/>
   </div>
 </template>
