ea205c6e2f0cffdb2280376dfc7c50ccf7687b7a
gaocn <gaowenwencn@163.com>
2022-01-28 00:30:09 +0800

    首页模块

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './config/index.js'
 import Vant from 'vant'
 // 加载vant全局样式
 import 'vant/lib/index.css'
